27c70e7bbf7c981a0060bc21b13d1885610c6561
Alan Griffiths <alan@octopull.co.uk>
2020-10-07 10:50:59 +0100

    Kill the server when the app exits

--- x11_kiosk_launch.sh
+++ x11_kiosk_launch.sh
@@ -10,9 +10,11 @@ unset WAYLAND_DISPLAY
 x11_display_file=$(mktemp)
 
 MIR_SERVER_ENABLE_X11=1 "${bindir}"mir_kiosk_x11 --x11-displayfd 5 5>"${x11_display_file}"&
+mir_kiosk_x11_pid=$!
 
 inotifywait --event close_write "${x11_display_file}"
 export DISPLAY
 DISPLAY=:$(cat "${x11_display_file}")
 rm "${x11_display_file}"
-XDG_SESSION_TYPE=mir GDK_BACKEND=x11 QT_QPA_PLATFORM=xcb SDL_VIDEODRIVER=x11 NO_AT_BRIDGE=1 exec "$@"
+XDG_SESSION_TYPE=mir GDK_BACKEND=x11 QT_QPA_PLATFORM=xcb SDL_VIDEODRIVER=x11 NO_AT_BRIDGE=1 "$@"
+kill $mir_kiosk_x11_pid
